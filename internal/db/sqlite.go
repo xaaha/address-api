62912b473185962d44dc5a10ff16500e5489148e
Pratik Thapa <pratik.philos@live.com>
2025-07-23 14:12:25 -0400

    using sqlite for db stuff

--- internal/db/sqlite.go
+++ internal/db/sqlite.go
@@ -1,1 +1,15 @@
 package db
+
+import (
+	"database/sql"
+	"path/filepath"
+)
+
+func CreateDB() (*sql.DB, error) {
+	dbLocation := filepath.Join("internal", "db", "data.db")
+	db, err := sql.Open("sqlite3", dbLocation)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
