42cd58dd45e85458f72b364303ecf3527a710d32
Pratik Thapa <pratik.philos@live.com>
2025-07-24 12:36:08 -0400

    minor changes on loader and migration

---
+++ internal/data/loader.go
@@ -0,0 +1,39 @@
+// Package data loads the json data to the sqlite db
+package data
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type address struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Address     string `json:"address"`
+	Phone       string `json:"phone"`
+	CountryCode string `json:"country-code"`
+	Country     string `json:"country"`
+}
+
+func ReadJSON() error {
+	// TODO: Loop over dir and rad all the files
+	file := "data/Afghanistan.json"
+	jsonByte, err := os.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	var testAddress []address
+	err = json.Unmarshal(jsonByte, &testAddress)
+	if err != nil {
+		return err
+	}
+
+	for _, value := range testAddress {
+		fmt.Println(value.Name)
+		fmt.Printf("Inserted %d records from %s\n", len(testAddress), file)
+	}
+
+	return nil
+}
--- internal/db/migrate.go
+++ internal/db/migrate.go
@@ -2,9 +2,7 @@
 package db
 
 import (
-	"encoding/json"
 	"fmt"
-	"os"
 	"path/filepath"
 
 	// sqlite3 driver
@@ -12,37 +10,6 @@ import (
 )
 
 // Should handle creating tables and inserting data
-
-type address struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Address     string `json:"address"`
-	Phone       string `json:"phone"`
-	CountryCode string `json:"country-code"`
-	Country     string `json:"country"`
-}
-
-// ReadJSON reads json for now
-func ReadJSON() error {
-	file := "internal/db/Afghanistan.json"
-	jsonByte, err := os.ReadFile(file)
-	if err != nil {
-		return err
-	}
-
-	var testAddress []address
-	err = json.Unmarshal(jsonByte, &testAddress)
-	if err != nil {
-		return err
-	}
-
-	for _, value := range testAddress {
-		fmt.Println(value.Name)
-		fmt.Printf("Inserted %d records from %s\n", len(testAddress), file)
-	}
-
-	return nil
-}
 
 // CreateDBAndTables creates sqlite tables and db
 func CreateDBAndTables() error {
