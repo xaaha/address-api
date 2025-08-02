225a7c935cadd90fc7a5f4e81f2bddd895fe85ba
Pratik Thapa <pratik.philos@live.com>
2025-08-02 17:28:26 -0400

    add read json in migrate.go

--- internal/db/migrate.go
+++ internal/db/migrate.go
@@ -7,12 +7,12 @@ import (
 
 	// sqlite3 driver
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/xaaha/address-api/internal/data"
 )
 
 // Should handle creating tables and inserting data
 
 // CreateDBAndTables creates sqlite tables and db
-// TODO: Update this to read the entire dir
 func CreateDBAndTables() error {
 	db, err := CreateDB()
 	if err != nil {
@@ -28,6 +28,19 @@ func CreateDBAndTables() error {
 	if err = ExecSQLFile(db, sqlPath); err != nil {
 		return err
 	}
+
+	addresses, err := data.ReadJSON("data")
+	if err != nil {
+		return err
+	}
+
+	for _, addr := range addresses {
+		if err = InsertAddress(db, addr); err != nil {
+			return err
+		}
+	}
+
+	fmt.Printf("Inserted %d addresses\n", len(addresses))
 
 	return nil
 }
