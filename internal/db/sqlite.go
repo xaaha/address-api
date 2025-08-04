64338342bf09ef81cb8119a55e717d09b338d04c
Pratik Thapa <pratik.philos@live.com>
2025-08-04 15:46:55 -0400

    add revivie toml and function comments

--- internal/db/sqlite.go
+++ internal/db/sqlite.go
@@ -10,6 +10,7 @@ import (
 
 // Should expose Connect() and return *sql.DB
 
+// CreateDB creates db named data
 func CreateDB() (*sql.DB, error) {
 	dbLocation := filepath.Join("internal", "db", "data.db")
 	db, err := sql.Open("sqlite3", dbLocation)
@@ -19,6 +20,7 @@ func CreateDB() (*sql.DB, error) {
 	return db, nil
 }
 
+// ExecSQLFile takes in db and .sql file and executes the statement
 func ExecSQLFile(db *sql.DB, path string) error {
 	sqlBytes, err := os.ReadFile(path)
 	if err != nil {
@@ -30,6 +32,7 @@ func ExecSQLFile(db *sql.DB, path string) error {
 	return err
 }
 
+// InsertAddress inserts address to the db created in CreateDB
 func InsertAddress(db *sql.DB, addr data.Address, path string) error {
 	sqlBytes, err := os.ReadFile(path)
 	if err != nil {
